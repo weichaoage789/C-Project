6b82b9d45a880226396676f254a427ffd2e58dad
wwc <weichao.emil>
2020-05-19 15:17:42 +0800

    添加lock工程

---
+++ 1、C#多线程/1、lock简单例子/LockDemo/LockDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LockDemo
+{
+    class Program
+    {
+        public static void thread1handle()
+        {
+            PrintStr("子线程1");
+        }
+        public static void thread2handle()
+        {
+            PrintStr("子线程2");
+        }
+        static void Main(string[] args)
+        {
+
+            ThreadStart threadStart1 = new ThreadStart(thread1handle);
+            Thread thread1 = new Thread(threadStart1);
+            thread1.Start();
+
+            ThreadStart threadStart2 = new ThreadStart(thread2handle);
+            Thread thread2 = new Thread(threadStart2);
+            thread2.Start();
+
+            Console.ReadKey();
+        }
+
+        private static readonly object obj = new object();
+        public static void PrintStr(string inputstr)
+        {
+            lock (obj)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(inputstr + "输出：" + i.ToString());
+                    Thread.Sleep(200);
+                }
+            }
+        }
+    }
+}
